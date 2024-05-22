e2631310f1522ba0943d2d346ebd4f01e8a3e3e6
unknown <khaledsaoud237@gmail.com>
2024-05-22 16:07:52 +0100

    tailwind website

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        velvet: "#bf1b2c",
+        comicbg: "#F3F3F3",
+      },
+      fontFamily: {
+        averox: ["Averox", "sans-serif"],
+        freya: ["Freya", "sans-serif"],
+        mechsuit: ["Mechsuit", "sans-serif"],
+      },
+      letterSpacing: {
+        m: "5px",
+      },
+    },
+  },
+};
